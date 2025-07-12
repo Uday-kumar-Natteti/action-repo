f71f73450478681c3f0e92adce263a36d7189b3c
Uday kumar <nuday9849@gmail.com>
2025-07-12 11:19:07 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 def main():
     print("Hello from action-repo!")
-    print("This file is used to test webhook triggers")
+    print("This file is used to test webhook triggers and stores in MongoDB database")
 
 if __name__ == "__main__":
     main()
